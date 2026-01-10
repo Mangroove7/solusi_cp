e4f91c1eb1a76579a4d71dbbfa1048a03d237584
Mangrove7 <morgantumcpp@gmail.com>
2026-01-10 08:43:34 +0700

    remove all dotfiles and .exe files from project

--- CodeForces/1348A.cpp
+++ CodeForces/1348A.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 
 void solve() {
-    //pertama kita tahu bahwa tiap bilangan 2^i -> N dipasangkan menjadi setengah-setengah
-    //dan kita tahu bahwa pasangannya berjumlah n/2 dimana N adalah genap
-    //untuk solusi optimal kita selalu memasangkan nilai paling maksimum dari deret 2^i yaitu 2^n
-    //ke deret kecil , dan sisanya yaitu deret besar ialah deret maks kita akan kurangkan
-    /*
+    /* pertama kita tahu bahwa tiap bilangan 2^i -> N dipasangkan menjadi setengah-setengah
+    dan kita tahu bahwa pasangannya berjumlah n/2 dimana N adalah genap
+    untuk solusi optimal kita selalu memasangkan nilai paling maksimum dari deret 2^i yaitu 2^n
+    ke deret kecil , dan sisanya yaitu deret besar ialah deret maks kita akan kurangkan
+    
     karena setidaknya untuk solusi minimal kita perlu membuat maks >= min (maks sedikit >= dibanding min)
     contoh n = 2 , maka akan terbentuk 2,4 pasangkan 4 sebagai max dan sisanya 2 menjadi min
     maka hasil = 2 */
     int n;cin >> n;
-    int maks = pow(2,n);
+    int maks = (1 << n);
     int min = 0;
     for (int i = 1; i <= n / 2 - 1; i++) {
 
-        maks += pow(2,i);
+        maks += (1 << i);
     }
     for (int i = n / 2; i <= n - 1; i++) {
         
-        min += pow(2,i);
+        min += (1 << i);
 
     }
     int sum = maks - min;
